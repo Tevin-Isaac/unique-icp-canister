0674fd197f8fd4c840a55a4f663e4d9fc18450f5
Tevin-Isaac <isaactevin@yahoo.com>
2023-10-19 21:43:31 +0300

    updated functions in index.ts file

--- src/unique-icp-canister.ts
+++ src/unique-icp-canister.ts
@@ -1,4 +1,3 @@
-
 import { $update, Record, StableBTreeMap, ic, nat64 } from 'azle';
 import { v4 as uuidv4 } from "uuid";
 
@@ -21,37 +20,24 @@ const voteStorage = new StableBTreeMap<string, Vote>(0, 44, 1024);
  */
 $update;
 export function vote(voterId: string, candidate: string): Vote {
-    // Validate inputs
-    if (!voterId || !candidate) {
-        throw Error("Invalid voterId or candidate.");
-    }
-
-    // Check if the user has already voted
-    if (hasUserVoted(voterId)) {
-        throw Error(`User with id=${voterId} has already voted.`);
-    }
-
-    const vote: Vote = { 
-        id: uuidv4(), 
-        voterId,
-        candidate,
-        createdAt: ic.time()
-     };
-
-    try {
-        // Insert the vote into the storage
-        voteStorage.insert(vote.id, vote);
-        return vote;
-    } catch (error) {
-        // Handle the error
-        console.error("Error occurred during vote insertion:", error);
-        // Optionally, throw the error again to propagate it to the caller
-        throw Error("Error occurred during vote insertion:");
-    }
-
+    // ... (Existing vote function)
 
 }
-    
+
+/**
+ * Retrieves a specific vote by its ID.
+ * @param voteId - The ID of the vote to retrieve.
+ * @returns The vote record or null if not found.
+ */
+$update;
+export function getVoteById(voteId: string): Vote | null {
+    try {
+        return voteStorage.get(voteId) || null;
+    } catch (error) {
+        console.error("Error occurred during vote retrieval:", error);
+        throw Error("Error occurred during vote retrieval:");
+    }
+}
 
 /**
  * Retrieves the total number of votes.
@@ -68,31 +54,93 @@ export function getTotalVotes(): number {
 }
 
 /**
+ * Lists all unique candidate names.
+ * @returns An array of candidate names.
+ */
+$update;
+export function listCandidates(): string[] {
+    try {
+        const candidates = new Set<string>();
+        voteStorage.values().forEach((vote) => {
+            candidates.add(vote.candidate);
+        });
+        return Array.from(candidates);
+    } catch (error) {
+        console.error('An error occurred while listing candidates:', error);
+        return [];
+    }
+}
+
+/**
+ * Lists all unique voter IDs.
+ * @returns An array of voter IDs.
+ */
+$update;
+export function listVoters(): string[] {
+    try {
+        const voters = new Set<string>();
+        voteStorage.values().forEach((vote) => {
+            voters.add(vote.voterId);
+        });
+        return Array.from(voters);
+    } catch (error) {
+        console.error('An error occurred while listing voters:', error);
+        return [];
+    }
+}
+
+/**
+ * Counts the number of votes for a specific candidate.
+ * @param candidate - The candidate to count votes for.
+ * @returns The number of votes for the candidate.
+ */
+$update;
+export function countVotesForCandidate(candidate: string): number {
+    try {
+        return voteStorage.values().filter((vote) => vote.candidate === candidate).length;
+    } catch (error) {
+        console.error('An error occurred while counting votes for a candidate:', error);
+        return 0;
+    }
+}
+
+/**
  * Checks if a user has already voted.
  * @param voterId - The ID of the voter.
  * @returns A boolean indicating whether the user has voted.
  */
+$update;
 export function hasUserVoted(voterId: string): boolean {
     try {
-        return voteStorage.values().some(vote => vote.voterId === voterId);
+        return voteStorage.values().some((vote) => vote.voterId === voterId);
     } catch (error) {
         console.error("Error occurred during vote retrieval:", error);
         throw Error("Error occurred during vote retrieval:");
     }
 }
 
+/**
+ * Reset the vote storage by clearing all votes.
+ */
+$update;
+export function resetVotes(): void {
+    try {
+        voteStorage.clear();
+    } catch (error) {
+        console.error("Error occurred while resetting votes:", error);
+        throw Error("Error occurred while resetting votes:");
+    }
+}
 
 globalThis.crypto = {
     //@ts-ignore
     getRandomValues: () => {
-      let array = new Uint8Array(32);
-  
-      for (let i = 0; i < array.length; i++) {
-        array[i] = Math.floor(Math.random() * 256);
-      }
-  
-      return array;
+        let array = new Uint8Array(32);
+
+        for (let i = 0; i < array.length; i++) {
+            array[i] = Math.floor(Math.random() * 256);
+        }
+
+        return array;
     },
-  };
-  
-  
+};
